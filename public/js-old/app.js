90d21269ecedd1bc9259db7f1a3c24f694a12ba4
jsumnersmith <jsumnersmith@gmail.com>
2016-01-30 09:41:54 -0500

    Updated style guide and renamed base components/templates with Boilerplate name.

--- public/js-old/app.js
+++ public/js-old/app.js
@@ -1,15 +1,10 @@
 //Main bootstrapping file.
 var React = require('react');
-var ReactDOM = require('react-dom');
 var App = require('./components/app.react');
-var BoilerplateAppApi = require('./data/BoilerplateAppApi');
 
 //Initially, load any data here.
-BoilerplateAppApi.load(function(data){
-  console.log(data);
-});
 
-ReactDOM.render(
+React.render(
   <App />,
   document.getElementById('app-mount')
 );
--- public/js/components/app.react.js
+++ public/js/components/app.react.js
@@ -1,54 +1,18 @@
 var React = require('react');
-var Store = require('../stores/appStore');
-var Actions = require('../actions/actions');
-
-//Load any subcomponents here.
-
-// Method to retrieve state from Stores
-function getAppState() {
-  return {
-    //Object reflecting stores:
-    // datum: Store.getDatum(),
-  };
-}
 
 // Define main Controller View
 var App = React.createClass({
 
-  // Get initial state from stores
-  getInitialState: function() {
-    return getAppState();
-  },
-
-  // Pulling in actions
-  customMethod: function(data){
-    return actions.receiveData(data);
-  },
-
-  // Add change listeners to stores
-  componentDidMount: function() {
-    Store.addChangeListener(this._onChange);
-  },
-
-  // Remove change listeners from stores
-  componentWillUnmount: function() {
-    Store.removeChangeListener(this._onChange);
-  },
-
   // Render our child components, passing state via props
   render: function() {
   	return (
-      <div className="rt-app">
-        //Load in sub-components here.
+      <div className="bp-app bp-grid">
+        <div className="bp-col-12">
+          <p>My App</p>
+        </div>
       </div>
   	);
-  },
-
-  // Method to setState based upon Store changes
-  _onChange: function() {
-    this.setState(getAppState());
   }
-
 });
 
-module.exports = FluxCartApp;
+module.exports = App;
